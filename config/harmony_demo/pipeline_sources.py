b94c230363722c17d6e01a1a520ad7504d79c67f
Abby Beeler <abby@zenysis.com>
2023-10-06 14:57:09 -0700

    fix harmony demo self serve configurations

--- config/harmony_demo/pipeline_sources.py
+++ config/harmony_demo/pipeline_sources.py
@@ -1,5 +1,6 @@
 from models.python.config.pipeline_sources_settings import PipelineSource
 
 PIPELINE_CONFIG = [
+    PipelineSource(source_id='self_serve', display_name='Self Serve'),
     PipelineSource(source_id='yellow_fever', display_name='Yellow Fever'),
 ]
