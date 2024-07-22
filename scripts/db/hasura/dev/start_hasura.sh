54b0627942c31976ad481af88a74c71643b96b24
Sybrand Strauss <sybrand@zenysis.com>
2024-07-22 11:14:37 +0200

    trying hasura in 5003 range

--- scripts/db/hasura/dev/start_hasura.sh
+++ scripts/db/hasura/dev/start_hasura.sh
@@ -35,7 +35,7 @@ if (( CONTAINER_COUNT == 0 )) ; then
       --name "${CONTAINER_NAME}" \
       -v "${HASURA_METADATA_DIR}:/hasura-metadata" \
       -v "${HASURA_CONTAINER_SCRIPTS_DIR}:/zenysis:ro" \
-      -p '9002:8080' \
+      -p '5003:8080' \
       --entrypoint 'sh' \
     "${IMAGE_NAME}"
 fi
